4fb599606c7b11313bf6b154a924883620598108
changang-4life <163348160+changang-4life@users.noreply.github.com>
2025-02-14 09:35:38 +1300

    v1

--- MGS Childcare.py
+++
@@ -1,6 +0,0 @@
-""" Program which will be used by a child daycare centre to keep track of
-children throughout the day
-Jade Akinbo
-14/02/25
-v1
-"""
---
+++ MGS Childcare/MGS-Childcare-v1.py
@@ -0,0 +1,11 @@
+""" Program which will be used by a child daycare centre to keep track of
+children throughout the day
+Jade Akinbo
+14/02/25
+v1
+"""
+
+def main():
+    """ Function that prints a welcome screen and menu selection for user """
+    print("𐙚‧₊˚📒✏️✩ ₊˚☁️⊹♡ Welcome to MGS Childcare 𐙚‧₊˚✏️📒✩ ₊˚☁️⊹♡")
+    
