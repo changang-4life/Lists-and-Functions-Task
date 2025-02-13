a07dc9198d332cabc293be1881e700ff99893be1
changang-4life <163348160+changang-4life@users.noreply.github.com>
2025-02-14 09:14:31 +1300

    gaga

--- MGS Childcare.py
+++ MGS Childcare.py
@@ -1,4 +1,4 @@
-"""Program which will be used by a child daycare centre to keep track of
+""" Program which will be used by a child daycare centre to keep track of
 children throughout the day
 Jade Akinbo
 14/02/25
