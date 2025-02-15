315fa89f39a656f0cd93f4a2ff3832774d272b31
changang-4life <163348160+changang-4life@users.noreply.github.com>
2025-02-15 14:52:40 +1300

    Version 1

--- MGS Childcare/MGS-Childcare-v1.py
+++ MGS Childcare/MGS-Childcare-v1.py
@@ -3,13 +3,13 @@ children throughout the day
 Jade Akinbo
 14/02/25
 
-v1 - main(): Main routine function
+v1 - main(): Main Routine function
  ⤷ Contains the main functionality of the program - all functions will be
  executed here
  ⤷ Needs to show a "Goodbye" message if the user wants to exit
 """
 
 def main():
-    """ Main Rx outine """
+    """ Main Routine """
     print("Goodbye")
 main()
